5ba4ed7c700ab9698c6026aa0ff4889401ef3f2c
Tom Elliott <tell029@aucklanduni.ac.nz>
2014-03-28 14:36:35 +1300

    first attempt at new updater for mac

---
+++ updateiNZightVIT-MAC.R
@@ -0,0 +1,39 @@
+# This is the update script for MAC
+library(RCurl)
+
+# First set up some global variables that will be used by the updater function
+isWindows <- FALSE
+isLinux <- FALSE
+isOSX <- TRUE
+.libPaths(Sys.getenv("R_LIBRARIES"))
+library(utils)
+OSstring <- "OSX"
+downloadMethod <- "curl"
+# We need to set the directory properly:
+setwd(strsplit(Sys.getenv("R_HOME"), "Library/Frameworks")[[1]][1])
+
+useOldR <- FALSE  # this can be set to TRUE for versions that need to
+                  # stick with an old version of R
+
+UPDATER_VERSION <- numeric_version(1.0)
+updater.location <- file.path(getwd(), "Library", "updateiNZightVIT.R")
+update.ext <- "-MAC"  # this will be "-WIN", "-MAC", "-MAC-snowleopard" etc...
+
+source_https <- function(url, ...) {
+  # load package
+    require(RCurl)
+
+  # Download the new file to a temporary location and source it.
+    sapply(c(url, ...), function(u) {
+        text <- getURL(u, followlocation = TRUE,
+                       cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+        ftmp <- tempfile()
+        con <- file(ftmp, open = "w")
+        writeLines(text, con)
+        close(con)
+        source(ftmp)
+    })
+}
+source_https("https://www.stat.auckland.ac.nz/~wild/downloads/iNZight/update.R")
+#source(file.choose())  # this is for debugging
+updateDistribution()
