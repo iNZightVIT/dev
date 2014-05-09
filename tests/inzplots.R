63ce09cf1e3c193ee4818a2787cafc8eee4502c8
Tom Elliott <tell029@aucklanduni.ac.nz>
2014-05-09 16:24:02 +1200

    some testing functnos

--- tests/inzplots.R
+++ tests/inzplots.R
@@ -9,44 +9,50 @@ count <- rpois(500, 10)
 
 upd();
 fs <- inzStructure("freq", count)
-pp <- iNZightPlot(height, armspan, sizeby = age, data = d)
+iNZightPlot(height, armspan, sizeby = age, data = d)
 
 upd()
 head(iNZightPlot(H, log(A), varnames = list(x = "height (cm)", freq = "count"), structure = fs)) -> p
 
 upd()
 d$height[10] <- Inf
-p <- iNZightPlot(height, armspan, colby = gender, data = d)
-p <- iNZightPlot(height, armspan, g2 = gender, g2.level = "male", data = d)
+iNZightPlot(height, armspan, colby = gender, data = d)
+iNZightPlot(height, armspan, g2 = gender, g2.level = "male", data = d)
 
 upd()
-iNZightPlot(height, armspan, g2 = cellsource, g2.level = 2, data = d)$missing
+iNZightPlot(height, armspan, g2 = cellsource, g2.level = 2, data = d)
 
 upd()
 ff <- as.factor(sample(1:200, 500, TRUE))
 iNZightPlot(ff)
 
 upd()
-iNZightPlot(height, armspan, g1 = cellsource, g1.level = 1:2, data = d)$missing
+iNZightPlot(height, armspan, g1 = cellsource, g1.level = 1:2, data = d)
 iNZightPlot(height, data = d, structure = fs)
 iNZightPlot(gender, data = d)
 
 upd()
 ipar <- inzpar(jitter = "x")
-iNZightPlot(height, armspan, data = d, inzpar = ipar)$toplot
-iNZightPlot(height, armspan, data = d, jitter = "y")$inzpar$jitter
-iNZightPlot(height, armspan, data = d)$inzpar$jitter
+iNZightPlot(height, armspan, data = d, inzpar = ipar)
+iNZightPlot(height, armspan, data = d, jitter = "y")
+iNZightPlot(height, armspan, data = d)
 
 upd()
-iNZightPlot(height, armspan, data = d)$toplot
-iNZightPlot(height, armspan, g1 = gender, data = d)$toplot
+iNZightPlot(height, armspan, g1 = cellsource, g2 = travel, g2.level = "_MULTI", data = d)
+iNZightPlot(height, armspan, data = d)
+iNZightPlot(height, armspan, g1 = gender, data = d)
 
 upd()
-iNZightPlot(height, armspan, data = d, structure = fs)$toplot$all -> eh
+iNZightPlot(height, armspan, data = d, structure = fs)
 
-
-with(eh, plot(x, y, cex = propsize))
 
 upd()
 iNZightPlot(height, armspan, data = d)
 iNZightPlot(height, armspan, g1 = gender, structure = fs, data = d)
+
+upd()
+iNZightPlot(height, armspan, data = d, cex.axis = 0.7)
+iNZightPlot(height, armspan, g1 = gender, g2 = cellsource, g2.level = "_MULTI", data = d)
+iNZightPlot(height, armspan, g1 = gender, g2 = cellsource, g2.level = "other", data = d)
+iNZightPlot(height, armspan, g1 = getlunch, g1.level = "none", data = d)
+iNZightPlot(height, g1 = getlunch, data = d)
