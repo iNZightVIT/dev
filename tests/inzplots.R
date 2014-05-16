11727aac213fd82e00a1f83a9f964fa40942b4a0
Tom Elliott <tomelliott90@gmail.com>
2014-05-16 16:10:14 +1200

    some more test code

--- tests/inzplots.R
+++ tests/inzplots.R
@@ -44,9 +44,29 @@ iNZightPlot(height, armspan, g1 = gender, data = d)$toplot
 upd()
 iNZightPlot(height, armspan, data = d, structure = fs)$toplot$all -> eh
 
-
-with(eh, plot(x, y, cex = propsize))
-
 upd()
 iNZightPlot(height, armspan, data = d)
-iNZightPlot(height, armspan, g1 = gender, structure = fs, data = d)
+iNZightPlot(height, armspan, g1 = gender, structure = fs, data = d, alpha = 0.5, colby = getlunch)
+
+upd()
+iNZightPlot(y=height, x=armspan, data=d, g1=gender, g2=getlunch, g2.level="_MULTI")
+
+
+
+grid.newpage(); pushViewport(viewport(width=0.5, height=0.5)); grid.rect()
+pushViewport(viewport(layout = grid.layout(2, 1, heights = unit(c(1, 5), "null"))))
+
+pushViewport(viewport(layout.pos.row = 1)); grid.rect(gp = gpar(fill = "yellow"))
+upViewport()
+pushViewport(viewport(layout.pos.row = 2, xscale = c(2, 8), yscale = c(10, 20)))
+
+grid.xaxis(); grid.yaxis()
+
+xax <- xaxisGrob(main = FALSE, label = FALSE)
+upViewport()
+pushViewport(viewport(layout.pos.row = 1))
+grid.draw(xax)
+
+
+## --- things to do:
+# - "sizeby" needs to be calculated RIGHT AT THE BEGINNING (using rescale() before it is split)
