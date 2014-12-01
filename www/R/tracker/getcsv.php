22b2b6e1f9643c46d7f71dec32ae3d29f9bf5709
Tom Elliott <tomelliott90@gmail.com>
2014-12-02 12:14:24 +1300

    script to generate CSV results

---
+++ www/R/tracker/getcsv.php
@@ -0,0 +1,29 @@
+<?php 
+// database info
+
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=userlocation.csv");
+
+include "con.php";
+
+$con = new mysqli($servername, $username, $password, $dbname);
+if ($con->connect_error) {
+  die();//"Connection failed: " . $con->connect_error);
+}
+
+if ($_GET['r'] != "imfromiNZight") {
+  die("There's nothing to show.");
+}
+
+$sql = "SELECT location, os FROM users";
+$result = $con->query($sql);
+
+echo "Location,OS\n";
+while($row = $result->fetch_assoc()) {
+  echo "\"" . $row["location"] . "\"" . "," . $row["os"] . "\n";
+}
+
+$con->close();
+
+
+?>
