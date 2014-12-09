272abe69895aa62479f6f0fe7e9e0a404191a2dd
Tom Elliott <tomelliott90@gmail.com>
2014-12-09 19:20:27 +1300

    added colour and own tites; deleted comments

--- www/R/tracker/plotUsers.R
+++ www/R/tracker/plotUsers.R
@@ -5,38 +5,29 @@ file <- read.csv("http://docker.stat.auckland.ac.nz/R/tracker/getcsv.php?r=imfro
                  header = TRUE, colClass = c("character", "factor"))
 cat(" done.\n")
 
-# loc.mat <- do.call(rbind, strsplit(file$Location, ","))
-# loc <- data.frame(lat = as.numeric(loc.mat[, 1]),
-#                   lon = as.numeric(loc.mat[, 2]))
-# 
-# os <- factor(ifelse(file$OS == "Windows", "1", "2"))
-
 cat("Loading libraries...")
 suppressPackageStartupMessages(library(ggplot2))
-#suppressPackageStartupMessages(library(rworldmap))
 cat(" done.\n")
 
 cat("Processing data...")
-file = file[!is.na(file$country) & file$country != "", ]  # get rid of missing
-userloc = as.data.frame(table(as.character(file[, "country"])))  # get freq
-levels(userloc$Var1)[levels(userloc$Var1) == "United Kingdom"] = "UK"
-levels(userloc$Var1)[levels(userloc$Var1) == "United States"] = "USA"
+file <- file[!is.na(file$country) & file$country != "", ]  # get rid of missing
+userloc <- as.data.frame(table(as.character(file[, "country"])))  # get freq
+levels(userloc$Var1) <-
+    sapply(levels(userloc$Var1), function(x)
+        switch(x, "United Kingdom" = "UK", "United States"  = "USA", x))
 cat(" done.\n")
 
 cat("Plotting data...")
-jpeg("userMap.jpg", width = 2000, height = 1200)
-world = map_data("world")
-ggplot() + geom_map(data = userloc, aes(map_id = Var1, fill = Freq), map = world) + expand_limits(x = world$long, y = world$lat) + geom_polygon(data = world, aes(x = long, y = lat, group = group), colour = "black", fill = NA)  + coord_equal() + labs(x = "Magnitude", y = "Latitude") + scale_fill_gradient(low = "grey", high ="black") + ggtitle("iNZight Downloaders") + theme(plot.title = element_text(lineheight=.8, face="bold", size = rel(2)))
-o = dev.off()
+jpeg("userMap.jpg", width = 1920, height = 1080)
+world <- map_data("world")
+ggplot() + geom_map(data = userloc, aes(map_id = Var1, fill = Freq), map = world) +
+    expand_limits(x = world$long, y = world$lat) +
+    geom_polygon(data = world, aes(x = long, y = lat, group = group), colour = "black", fill = NA) +
+    coord_equal() +
+    labs(x = sprintf("\n\nMap generated on %s", date()), y = "") +
+    scale_fill_gradient(low = "#DDFFDD", high ="#005500") +
+    ggtitle("iNZight Users\n") +
+    theme(plot.title = element_text(lineheight = 0.8, face = "bold", size = rel(2)))
+o <- dev.off()
 cat(" done.\n")
 
-
-
-# newmap <- getMap(res = "low")
-# jpeg("userMap.jpg", width = 2000, height = 1200)
-# plot(newmap)
-# points(loc$lon, loc$lat, col = c("blue", "red")[os], cex = 0.6, pch = 4, lwd = 2)
-# legend("topleft", c("Windows", "Mac"), col = c("blue", "red"), pch = 4, bty = "n")
-# title(xlab = sprintf("Map generated on %s", date()), cex.lab = 2)
-# o <- dev.off()
-# cat(" done.\n")
