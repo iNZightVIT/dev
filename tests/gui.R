c9f9eb4af30da881a257382cfd9d2a845001fd4b
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-03-31 14:27:29 +1300

    testsing nwe plots and inzight

--- tests/gui.R
+++ tests/gui.R
@@ -1,13 +1,7 @@
+q()
 library(devtools)
 
 load_all("~/iNZight/iNZight")
 gui <- iNZGUI$new()
 gui$initializeGui(read.csv("~/iNZight/data/Census at School-500.csv",
                            header = TRUE, comment.char = "#"))
-
-
-
-
-
-library(gWidgets2)
-dispose(w); 
--- tests/newplots.R
+++ tests/newplots.R
@@ -8,3 +8,10 @@ document(pkg)
 check_doc(pkg)
 check(pkg)
 
+
+load_all(pkg)
+iNZightPlot(x = Sepal.Width, y = NULL, varnames = list(x = "Sepal Width", y = "oops"), data = iris)
+
+getPlotSummary(x = Sepal.Width, y = NULL, varnames = list(x = "Sepal Width", y = "oops"), data = iris)
+
+getPlotSummary(x = Sepal.Width, data = iris, inference.type = "conf", summary.type = "inference", bs.inference = TRUE)
