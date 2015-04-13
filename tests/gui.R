736fb326dce494033c1ac2d989c638e4dd1ae09d
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-04-13 15:17:34 +1200

    test code

--- tests/gui.R
+++ tests/gui.R
@@ -1,10 +1,17 @@
 q()
 
+setwd("~/iNZight/iNZight")
 library(devtools)
 load_all("~/iNZight/iNZightPlots")
+load_all("~/iNZight/iNZightMaps")
+load_all("~/iNZight/iNZightModules")
 load_all("~/iNZight/iNZight")
 gui <- iNZGUI$new()
-#gui$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
-#                                header = TRUE, comment.char = "#"))
+gui$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
+                                header = TRUE, comment.char = "#"))
+
 gui$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
+                                header = TRUE, comment.char = "#"))
+
+gui$initializeGui(D <- read.csv("~/Downloads/NZ_earthquakes2010.csv",
                                 header = TRUE, comment.char = "#"))
