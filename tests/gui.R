62d76918bc3541a879f226c84669271d981cd726
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-04-15 10:25:10 +1200

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -2,18 +2,18 @@ q()
 
 setwd("~/iNZight/iNZight")
 library(devtools)
+
 load_all("~/iNZight/iNZightPlots")
-load_all("~/iNZight/iNZightMaps")
-load_all("~/iNZight/iNZightModules")
-
 load_all("~/iNZight/iNZight")
-try(dispose(gui$win), TRUE)
-gui <- iNZGUI$new()
-gui$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
-                                header = TRUE, comment.char = "#"))
+try(dispose(KK$win), TRUE)
+KK <- iNZGUI$new()
+KK$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
+                               header = TRUE, comment.char = "#"))
 
-gui$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
-                                header = TRUE, comment.char = "#"))
+KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
+                               header = TRUE, comment.char = "#"))
 
-gui$initializeGui(D <- read.csv("~/Downloads/NZ_earthquakes2010.csv",
-                                header = TRUE, comment.char = "#"))
+
+
+KK$initializeGui(D <- read.csv("~/Downloads/NZ_earthquakes2010.csv",
+                               header = TRUE, comment.char = "#"))
