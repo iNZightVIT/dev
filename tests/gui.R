df8914ecb1d456f91a76a4190355c19a697b311b
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-04-15 15:45:33 +1200

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -7,13 +7,11 @@ load_all("~/iNZight/iNZightPlots")
 load_all("~/iNZight/iNZight")
 try(dispose(KK$win), TRUE)
 KK <- iNZGUI$new()
-KK$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
-                               header = TRUE, comment.char = "#"))
-
 KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
                                header = TRUE, comment.char = "#"))
 
-
+KK$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
+                               header = TRUE, comment.char = "#"))
 
 KK$initializeGui(D <- read.csv("~/Downloads/NZ_earthquakes2010.csv",
                                header = TRUE, comment.char = "#"))
