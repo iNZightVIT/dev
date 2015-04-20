756c3844dac249375900f7f527c3d17c7d9c8c52
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-04-20 15:07:47 +1200

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -7,6 +7,7 @@ load_all("~/iNZight/iNZightPlots")
 load_all("~/iNZight/iNZight")
 try(dispose(KK$win), TRUE)
 KK <- iNZGUI$new()
+
 KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
                                header = TRUE, comment.char = "#"))
 
@@ -15,3 +16,11 @@ KK$initializeGui(D <- read.csv("~/iNZight/data/Gap Minder Data.csv",
 
 KK$initializeGui(D <- read.csv("~/Downloads/NZ_earthquakes2010.csv",
                                header = TRUE, comment.char = "#"))
+
+
+KK$initializeGui(D <- read.csv("~/Downloads/sampleLoan20072011.csv", header = TRUE))
+
+load_all("~/iNZight/iNZightPlots")
+iNZightPlot(loan_amnt, funded_amnt, data = D, colby = mths_since_last_major_derog)
+
+D$mths_since_last_major_derog
