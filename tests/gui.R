09ace730562a9bb916faf57a45546d8ac1e450bf
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-06-17 15:21:22 +1200

    testingsurvey

--- tests/gui.R
+++ tests/gui.R
@@ -8,7 +8,7 @@ load_all("~/iNZight/iNZightMR")
 
 
 load_all("~/iNZight/gWidgets2RGtk2")
-
+library(survey); data(api)
 
 #system("cp /home/tell029/R/x86_64-pc-linux-gnu-library/3.1/id.txt ~/iNZight/dev/tests/old_id.txt")
 
@@ -17,6 +17,12 @@ load_all("~/iNZight/iNZightPlots")
 load_all("~/iNZight/iNZight")
 KK <- iNZGUI$new()
 
+
+KK$initializeGui(apiclus2)
+
+dstrat<-svydesign(id=~1,strata=~stype, weights=~pw, data=apistrat, fpc=~fpc)
+
+
 KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
                                header = TRUE, comment.char = "#"))
 
@@ -24,28 +30,5 @@ KK$initializeGui(D <- read.csv("~/iNZight/data/FutureLearn/Gapminder.csv",
                                header = TRUE, comment.char = "#"))
 
 
-
-curMenu <- svalue(KK$menubar)
-curMenu[["Plot"]] <- list(
-    gaction("New", handler = function(h, ...) gmessage("ok")),
-    gaction("Close", handler = function(h, ...) gmessage("done."))
-    )
-svalue(KK$menubar) <- curMenu
-
-
-
-########## Test
-
-w <- gwindow()
-gtop <- ggroup(FALSE, cont = w)
-menugrp <- ggroup(cont = gtop)
-men <- list("File" = list(gaction("Open", handler = function(h, ...) gmessage("open."))))
-q <- gmenu(men, cont = menugrp)
-
-men2 <- c(svalue(q),
-          list("Plot" = list(gaction("Close", handler = function(h, ...) dispose(w)))))
-svalue(q) <- men2
-
-men3 <- c(svalue(q)[[1]],
-          list("Plot" = list(gaction("Open", handler = function(h, ...) dispose(w)))))
-svalue(q) <- men3
+iNZightPlot(api99, api00, data = apistrat, trend = "linear")
+iNZightPlot(api99, api00, design = dstrat, trend= "linear")
