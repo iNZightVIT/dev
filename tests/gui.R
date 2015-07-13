fd710aa5a4fcb0ef791bcb56374d6684bc12cf11
Tom Elliott <tell029@aucklanduni.ac.nz>
2015-07-13 14:10:39 +1200

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -21,9 +21,21 @@ KK <- iNZGUI$new()
 KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv",
                                header = TRUE, comment.char = "#"))
 
-KK$initializeGui(D <- read.csv("~/iNZight/data/FutureLearn/Gapminder.csv",
+KK$initializeGui(D2 <- read.csv("~/iNZightg/data/FutureLearn/Gapminder.csv",
                                header = TRUE, comment.char = "#"))
 
 
 iNZightPlot(api99, api00, data = apistrat, trend = "linear") -> pl
 pl
+
+
+
+
+load_all("~/iNZight/iNZightPlots")
+iNZightPlot(getlunch, data = D,  inference.type = c("comp", "conf"))
+iNZightPlot(getlunch, gender, data = D, zoombars = c(1, 3),  inference.type = c("comp", "conf"))
+
+load_all("~/iNZight/iNZightPlots")
+iNZightPlot(Country, data = D2)
+
+
