eed0fb5b16533a93885efe888fdbf68bca62e1b6
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-12-15 11:03:46 +1300

    testing maps

---
+++ tests/maps.R
@@ -0,0 +1,19 @@
+###library(iNZightMaps)
+devtools::document("~/iNZight/iNZightMaps")
+devtools::load_all("~/iNZight/iNZightMaps", export_all = FALSE)
+quakes <- read.csv("~/iNZight/data/QuakesNZ2000.csv", header = TRUE)
+
+map.obj <- iNZightMap(~Latitude, ~Longitude, data = quakes)
+plot(map.obj)
+
+?iNZightMap
+
+
+
+
+library(cairoDevice)
+Cairo()
+library(iNZightMaps)
+NZQuakes = read.csv("~/iNZight/data/QuakesNZ2000.csv", header = T)
+obj <- iNZightMap(~Latitude, ~Longitude, NZQuakes)
+plot(obj)
