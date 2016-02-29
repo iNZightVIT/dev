deeec4c8e9baa135b969476d88ceffee41ad8bce
Tom Elliott <tom.elliott@auckland.ac.nz>
2016-03-01 11:37:14 +1300

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -8,16 +8,15 @@ library(devtools)
 ## Load - generic
 try(dispose(KK$win), TRUE)
 #load_all("~/iNZight/iNZightTools", export_all = FALSE)
-load_all("~/iNZight/iNZightPlots", export_all = FALSE)
-load_all("~/iNZight/iNZightModules", export_all = FALSE)
+#load_all("~/iNZight/iNZightPlots", export_all = FALSE)
+#load_all("~/iNZight/iNZightModules", export_all = FALSE)
 load_all(export_all = FALSE)
 
 ## Run - generic
 KK <- iNZGUI$new()
-KK$initializeGui(D <- read.csv("~/iNZight/data/Census at School-500.csv", header = TRUE, comment.char = "#"))
+KK$initializeGui()#D <- read.csv("~/iNZight/data/Census at School-500.csv", header = TRUE, comment.char = "#"))
 
 TRUE
-
 
 
 #census.at.school.500 <- read.csv("~/iNZight/data/Census at School-500.csv", header = TRUE, comment.char = "#")
