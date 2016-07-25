4f03a9bfa41b6877dd3f65cda0d28a6e26023ba4
Tom Elliott <tomelliottnz@gmail.com>
2016-07-26 09:31:37 +1200

    fix ls command

--- scripts/mac_diagnosis.sh
+++ scripts/mac_diagnosis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls -al /Library/Frameworks && ls -al /Library/Frameworks/R.framework/Versions && ls -al /Applications/iNZightVIT | pbcopy
+ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT | pbcopy
 
 echo Info copied to clipboard, please paste in email.
