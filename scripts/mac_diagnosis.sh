71e3f086fbbf4ba6062475c368ce195260a97217
Tom Elliott <tomelliottnz@gmail.com>
2016-07-26 09:33:59 +1200

    save to log file

--- scripts/mac_diagnosis.sh
+++ scripts/mac_diagnosis.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT | pbcopy
+ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT > ~/Desktop/iNZight.log
 
 echo Info copied to clipboard, please paste in email.
