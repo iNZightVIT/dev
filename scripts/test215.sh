b5d1896cb69198bb6cba81a05a800704e4936e5f
Tom Elliott <tomelliottnz@gmail.com>
2016-09-02 08:54:42 +1200

    clean out old files

--- scripts/test215.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-~/R-2.15.3/bin/R CMD INSTALL -l ~/iNZight/tmp ~/iNZight/vit
-tar czvf ~/iNZight/tmp/vit215.tgz ~/iNZight/tmp/vit
-scp ~/iNZight/tmp/vit215.tgz inzight@docker.stat.auckland.ac.nz:www/R/test/
-rm -rf ~/iNZight/tmp/vit*
