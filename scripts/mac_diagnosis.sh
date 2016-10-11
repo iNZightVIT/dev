3d716b14dd9cc3657204fee5984b795a90bfc6bc
Tom Elliott <tomelliottnz@gmail.com>
2016-10-11 17:09:49 +1300

    Add `pkgutil --packages` to log output

--- scripts/mac_diagnosis.sh
+++ scripts/mac_diagnosis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT > ~/Desktop/iNZight.log
+ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT /Applications/R* > ~/Desktop/iNZight.log
+echo "\nInstalled packages:" >> ~/Desktop/iNZight.log
+pkgutil --packages >> ~/Desktop/iNZight.log
 
 echo Log file created on the desktop. Please attach to email.
