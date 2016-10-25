0b711a2d2b3458ad3195079cd664b2febe8344ed
Tom Elliott <tomelliottnz@gmail.com>
2016-10-25 15:11:08 +1300

    Add R library to output

--- scripts/mac_diagnosis.sh
+++ scripts/mac_diagnosis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT /Applications/R* > ~/Desktop/iNZight.log
+ls -al /Library/Frameworks /Library/Frameworks/R.framework/Versions /Applications/iNZightVIT /Applications/iNZightVIT/.library /Applications/R* > ~/Desktop/iNZight.log
 echo "\nInstalled packages:" >> ~/Desktop/iNZight.log
 pkgutil --packages >> ~/Desktop/iNZight.log
 
