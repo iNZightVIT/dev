b72e0aa236caf93d7d57c20f3c9c029573219734
Tom Elliott <tomelliottnz@gmail.com>
2016-12-14 20:48:51 +1300

    Allow patching.

--- updateProfile-windows.R
+++ updateProfile-windows.R
@@ -1,6 +1,6 @@
 ## UPDATE iNZightVIT:
 
-VERSION = 1.0
+VERSION = 1.1
 
 Sys.setenv("R_HOME" = file.path(getwd()))
 .libPaths(file.path(getwd(), "library"))
@@ -9,4 +9,14 @@ Sys.setenv("R_HOME" = file.path(getwd()))
 lib <- file.path("~", "iNZightVIT", "modules")
 if (file.exists(lib)) .libPaths(lib)
 
-source("https://raw.githubusercontent.com/iNZightVIT/dev/master/update.R")
+patches <- list.files("../", pattern = "patch$")
+if (length(patches) > 0) {
+  cat("Updating iNZight from patch file:", patches[1], "\n")
+  utils::unzip(sprintf("../%s", patches[1]), exdir = "library")
+  cat("\nPatching complete.\n")
+  
+  Sys.sleep(3)
+  q("no")
+} else {
+  source("https://raw.githubusercontent.com/iNZightVIT/dev/master/update.R")
+}
