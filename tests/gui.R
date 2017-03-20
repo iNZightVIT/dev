f77545a2f23c7d7703d0bf96b3f913d53c59155a
Tom Elliott <tomelliottnz@gmail.com>
2017-03-20 20:23:41 +1300

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -11,11 +11,11 @@ library(devtools)
 try(dispose(KK$win), TRUE)
 #load_all("~/iNZight/gWidgets2RGtk2", export_all = FALSE)
 #load_all("~/iNZight/iNZightRegression", export_all = FALSE)
-load_all("~/iNZight/iNZightTools", export_all = FALSE)
+#load_all("~/iNZight/iNZightTools", export_all = FALSE)
 #load_all("~/iNZight/iNZightTS", export_all = FALSE)
 load_all("~/iNZight/iNZightPlots", export_all = FALSE)
 load_all("~/iNZight/iNZightModules", export_all = FALSE)
-load_all(export_all = FALSE)
+if (! "package:iNZight" %in% search()) load_all(export_all = FALSE)
 data(census.at.school.500, package = 'iNZight')
 ##data(gapminder, package = "iNZight")
 ##data(api, package="survey")
@@ -30,8 +30,7 @@ KK$initializeGui(census.at.school.500)
 NULL
 
 
-
-
+font(KK$plotWidget$plotNb["Model Output"]) <- list(size=10, family="monospace")
 
 
 
