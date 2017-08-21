4b7f15ca535071fc2ec45410925a4e046802d2d6
Tom Elliott <tomelliottnz@gmail.com>
2017-08-21 17:49:37 +1200

    testing

--- tests/regmod.R
+++ tests/regmod.R
@@ -4,14 +4,29 @@ load_all("~/iNZight/iNZightRegression")
 data("census.at.school.500", package = "iNZight")
 dat <- census.at.school.500
 
-load_all("~/iNZight/iNZightModules")
+fit <- lm(height ~ armspan + gender + sqrt(age) + relevel(travel, "other"),
+          data = dat)
+fit2 <- glm(gender ~ armspan + travel + sqrt(age), data = dat,
+            family = binomial)
 
-variables <- inzexp()
-variables <- c(variables, inzexp("gender"))
-variables <- c(variables, inzexp("height", "log"))
-variables <- c(variables, inzexp("armspan", "power", 2))
+load_all("~/iNZight/iNZightRegression")
+plotlm6(fit, 7)
+plotlm6(fit2, 7)
 
-variables
-tostring(variables)
 
-names(variables)
+bs.fits <- replicate(30, {
+    update(fit, subset = sample(nrow(dat), replace = TRUE))
+}, simplify = FALSE)
+
+update(fit, subset = sample(nrow(dat), replace = TRUE))
+
+boot <- function(fit, N = 20, ...) UseMethod("boot")
+boot.lm <- function(fit, N = 20, ...) {
+    X <- model.matrix(fit)
+    ii <- sample(nrow(X), replace = TRUE)
+    Yi <- X[ii, 1]
+    Xi <- X[ii, -1]
+    Bhat <- solve(t(Xi) %*% Xi, t(Xi) %*% Yi)
+    Yhat <- Xi %*% Bhat
+    res <- Yi - Yhat
+}
