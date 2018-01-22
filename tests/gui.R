bd06f9a20c0daad6377bc125d59bb9164abbdf9c
Tom Elliott <tomelliottnz@gmail.com>
2018-01-22 15:31:06 +1300

    testing

--- tests/gui.R
+++ tests/gui.R
@@ -14,7 +14,7 @@ try(dispose(KK$win), TRUE)
 load_all("~/iNZight/iNZightTools", export_all = FALSE)
 #load_all("~/iNZight/iNZightTS", export_all = FALSE)
 #load_all("~/iNZight/iNZightPlots", export_all = FALSE)
-load_all("~/iNZight/iNZightModules", export_all = FALSE)
+#load_all("~/iNZight/iNZightModules", export_all = FALSE)
 #if (! "package:iNZight" %in% search())
 load_all(export_all = FALSE)
 data(census.at.school.500, package = 'iNZight')
@@ -23,17 +23,31 @@ data(census.at.school.500, package = 'iNZight')
 
 ## Run - generic
 KK <- iNZGUI$new()
-##KK$initializeGui()
+KK$initializeGui()
 ##KK$initializeGui(apiclus2)
-KK$initializeGui(census.at.school.500)
+##KK$initializeGui(census.at.school.500)
 ##KK$initializeGui(gapminder)
 
 NULL
 
+w <- KK$ctrlWidget$ctrlGp$children[[1]]
+w[6, ] <- NULL
+xx <- which(sapply(KK$ctrlWidget$ctrlGp$children[[1]]$child_positions, function(x) x$x == 5))
+
+KK$ctrlWidget$ctrlGp$children[[1]]$remove_child(KK$ctrlWidget$ctrlGp$children[[1]]$child_positions[[xx[1]]]$child)
 
 
 try(dispose(w))
 w <- gwindow()
+g <- ggroup(cont = w)
+t <- glayout(cont = g)
+lbl <- glabel('hello world')
+t[1, 1] <- lbl
+add(g, t)
+
+delete(t, lbl)
+
+
 t <- gtable(data.frame(Levels = 1:10), cont=w)
 t$get_items()
 
