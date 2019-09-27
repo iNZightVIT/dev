8abe4d41c16037813ab775dd90197b1fe4d8ae0a
Tom Elliott <tomelliottnz@gmail.com>
2019-09-27 14:26:50 +1200

    Ignore case for windows

--- rename_binaries.R
+++ rename_binaries.R
@@ -2,7 +2,10 @@ pkg <- basename(getwd())
 rv <- as.character(getRversion())
 cat(sprintf("Renaming files for %s with R version %s\n", pkg, rv))
 for (ext in c(".zip", ".tgz")) {
-    file <- list.files(pattern = sprintf("%s.+%s", pkg, ext))
+    file <- list.files(
+        pattern = sprintf("%s.+%s", pkg, ext),
+        ignore.case = TRUE
+    )
     if (length(file)) {
         file2 <- gsub(ext, paste0("_R_", rv, ext), file)
         cat(sprintf("Renaming %s -> %s\n", file, file2))
