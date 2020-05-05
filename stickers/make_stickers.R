2378c0228853f5c9d29c8ef1eb23b0fc5c702655
Tom Elliott <tomelliottnz@gmail.com>
2020-05-05 13:22:58 +1200

    play around with hexSticker pkg

---
+++ stickers/make_stickers.R
@@ -0,0 +1,15 @@
+library(hexSticker)
+
+# iNZight
+imgurl <- system.file("images/inzight_transp.png", package="iNZight")
+sticker(imgurl,
+    package="",
+    h_fill = "white",
+    h_color = "#094b85",
+    p_color = "#094b85",
+    p_size = 20,
+    s_x = 1,
+    s_y = 0.8,
+    s_width = .9,
+    filename="stickers/inzight.png"
+)
